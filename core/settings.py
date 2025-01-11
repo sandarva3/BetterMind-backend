690fd1ea8919fe5c4523997b879d519ce398bd68
sandarva3 <sandarvapoudel182@gmail.com>
2025-01-11 13:02:20 +0545

    models developed

--- core/settings.py
+++ core/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'solace',
 
-    
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'solace.CustomUser'
--- solace/models.py
+++ solace/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    fullname = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.username
+
+
+
+class User(CustomUser):
+    pass
+
+
+
+class Prof(CustomUser):
+    verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+
+
+
+class UserData(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userdata')
+    questionNo = models.IntegerField()
+    questionText = models.TextField()
+    answer = models.TextField()
+
+    def __str__(self):
+        return f"Userdata of {self.user.username}"
+    
+
+
+class ProfData(models.Model):
+    prof = models.OneToOneField(Prof, on_delete=models.CASCADE, related_name='profdata')
+    questionNo = models.IntegerField()
+    questionText = models.TextField()
+    answer = models.TextField()
+
+    def __str__(self):
+        return f"Profdata of {self.prof.username}"
